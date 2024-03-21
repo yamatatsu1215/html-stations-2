9a1a3e3a77b148ef7eb242baee24cc3ed88b7f8f
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-03-21 22:53:33 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,12 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  var flag = false;
+  if (document.getElementById("check").checked) {
+    flag = true;
+    document.getElementById("text").style.backgroundColor = 'red';
+  }
+  else{
+    flag = false;
+    document.getElementById("text").style.backgroundColor = '#ffffff';
+  }
 }
